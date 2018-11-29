d7a4ca39e8e15470d90066684c87005b3ff4b35e
bingr001 <bingr001@gmail.com>
2018-11-29 06:17:39 -0800

    re-added profile image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,68 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import pedro from '../images/pedroprofile.png'
 
 
 
 
-const Header = (props) => (
-    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
-        <div className="logo">
-            <span className="icon fa-diamond"></span>
-        </div>
-        <div className="content">
-            <div className="inner">
-                <h1>Pedro LaTorre</h1>
-   
-                <p>You're Not Alone </p>
-            </div>
-        </div>
-        <nav>
-            <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Rest LA</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>Speaking</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
-            </ul>
-        </nav>
-    </header>
+const Header = props => (
+  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+    <div className="logo">
+      <span className="icon fa-diamond" />
+    </div>
+    <div className="content">
+      <div className="inner">
+        <h1>Pedro LaTorre</h1>
+        <span className="image main"> <img src={pedro} alt="" /></span>
+
+        <p>You're Not Alone </p>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('intro')
+            }}
+          >
+            About
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('work')
+            }}
+          >
+            Rest LA
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('about')
+            }}
+          >
+            Speaking
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('contact')
+            }}
+          >
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </header>
 )
 
 Header.propTypes = {
