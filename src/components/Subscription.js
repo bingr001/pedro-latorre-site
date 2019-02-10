5bc4dc9fa6c9fb0e173e6695b80c4061176fabd3
Brent Ingram <bingr001@users.noreply.github.com>
2019-02-10 00:00:44 -0800

    set up forms

---
+++ src/components/Subscription.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Subscription = (props) => (
+<form name="contact" method="post" action="/success" data-netlify="true" data-netlify-honeypot="bot-field">
+  <h2>Request More Info</h2>
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+      <label>Your Phone Number: <input type="text" name="phone"/> </label>
+  </p>
+  <p>
+      <label>Church Website: <input type="text" name="website"/> </label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <div data-netlify-recaptcha="true"></div>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+)
+
+
+export default Subscription
--- src/pages/brave-work.js
+++ src/pages/brave-work.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+
 import Layout from '../components/layout'
 import Footer from '../components/Footer'
+import Subscription from '../components/Subscription'
+
 
 
 const BraveWork = () => (
   <Layout>
     <h1>Brave Work</h1>
-    <p>Welcome to page 2</p>
+    <p>Brave Work information</p>
     <Link to="/">Go back to the homepage</Link>
+    <Subscription/> 
     <Footer/>
-
   </Layout>
 )
 
