68799ceed677abadd7d3a0fda9cbf2b78789328a
Brent Ingram <bingr001@users.noreply.github.com>
2019-02-11 01:51:58 -0800

    Updated Book modal

--- src/components/Main.js
+++ src/components/Main.js
@@ -34,7 +34,15 @@ class Main extends React.Component {
         <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">The Middle Seat</h2>
           <span className="image main"><img src={themiddleseat} alt="" /></span>
-          <p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
+          <p>The Middle Seat started as a moment. A moment that forced me into the dreaded middle seat on Delta Flight 940 from Oregon to Florida. I had been out on the road drumming and speaking for quite some time. I was tired and would have strapped a massive "DO NOT DISTURB" sign to my forehead, if I could have. I was hoping that my mileage status would bump me into first class and keep me far from talkative humans. Don’t you judge me! You know you have had similar feelings! All of my prayers went unanswered as I squeezed into the middle seat. I was uncomfortable, tired, and pretty grumpy. But this single red eye flight would change my perspective on people forever. I believe all of us are in middle seat moments everyday. Whether it’s the grocery store or the gym we all have the ability to be the answer to someone's prayer. 
+            <br/>
+            <br/>
+            We have the ability to be the friend that someone has been desperate to know. The middle seat is not about saying the right things or being overly spiritual, it’s about learning to see average moments as miracles waiting to happen. I open the corridors of my heart in this book and unfortunately it’s not all pretty. I needed this middle seat moment to change my perspective on God and people. What if life was more than building your brand? What if your platform doesn’t need to be reached but needs to be realized? What if we stopped craving platforms and started loving people?</p>
+            <br/>
+            Purchase The Middle Seat below!
+            <a href="https://www.amazon.com/Middle-Seat-Craving-Platforms-Loving/dp/1483572986/ref=sr_1_2?ie=UTF8&qid=1468983289&sr=8-2&keywords=pedro+latorre">Amazon</a>
+            <br/>
+            <a href="https://www.barnesandnoble.com/w/the-middle-seat-pedro-latorre/1124125010?ean=9781483572987">Barnes and Noble</a>
           {close}
         </article>
 
