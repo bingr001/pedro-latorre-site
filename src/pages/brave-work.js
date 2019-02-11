d5d767acdd72bd7dcec3d64448c9c3744b682d3e
Brent Ingram <bingr001@users.noreply.github.com>
2019-02-10 21:15:43 -0800

    removed layout div

--- src/pages/brave-work.js
+++ src/pages/brave-work.js
@@ -2,38 +2,37 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 
-import Layout from '../components/layout'
 import Footer from '../components/Footer'
 //import Subscription from '../components/Subscription'
 
 
 
 const BraveWork = () => (
-  <Layout>
+    <div>
     <h1>Brave Work</h1>
     <p>Brave Work information</p>
     <form name="contactsample" method="POST" data-netlify="true">
             <input type="hidden" name="contactsample" value="contact" />
-          <div class="fields">
-            <div class="field half">
+          <div className="fields">
+            <div className="field half">
               <input type="text" name="name" id="name" placeholder="Name" />
             </div>
-            <div class="field half">
+            <div className="field half">
               <input type="email" name="email" id="email" placeholder="Email" />
             </div>
-            <div class="field">
+            <div className="field">
               <textarea name="message" id="message" placeholder="Message" rows="7"></textarea>
             </div>
-            <div class="field">
+            <div className="field">
               <div 
               data-netlify-recaptcha="true"></div>
             </div>
           </div>
-          <ul class="actions">
-            <li><input type="submit" value="Send Message" class="button primary" /></li>
+          <ul className="actions">
+            <li><input type="submit" value="Send Message" className="button primary" /></li>
           </ul>
 			</form>
-  </Layout>
+      </div>
 )
 
 export default BraveWork
