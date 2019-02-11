cf7d221db6d20dbbab03fd01bfcc5e39b48e9674
Brent Ingram <bingr001@users.noreply.github.com>
2019-02-11 09:01:42 -0800

    removed dead social links and added instagram

--- src/components/Main.js
+++ src/components/Main.js
@@ -68,10 +68,8 @@ class Main extends React.Component {
             </ul>
           </form>
           <ul className="icons">
-            <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-            <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
-            <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
-            <li><a href="#" className="icon fa-github"><span className="label">GitHub</span></a></li>
+            <li><a href="https://www.instagram.com/pedrolatorre_/" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
+ 
           </ul>
           {close}
         </article>
