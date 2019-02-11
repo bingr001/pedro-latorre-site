f7e9ff3fa4e2a346e28a3fe3ce1898e4211c76cc
Brent Ingram <bingr001@users.noreply.github.com>
2019-02-10 22:29:56 -0800

    tried to do subscription import

--- src/components/Subscription.js
+++ src/components/Subscription.js
@@ -1,28 +1,29 @@
 import React from 'react'
 
 const Subscription = (props) => (
-<form name="contact" method="post" action="/success" data-netlify="true" data-netlify-honeypot="bot-field">
-  <h2>Request More Info</h2>
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-      <label>Your Phone Number: <input type="text" name="phone"/> </label>
-  </p>
-  <p>
-      <label>Church Website: <input type="text" name="website"/> </label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <div data-netlify-recaptcha="true"></div>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+  <form name="contact2" method="post" action="/success" data-netlify="true" data-netlify-honeypot="bot-field">
+    <input type="hidden" name="contact2" value="contact2" />
+    <h2>Request More Info</h2>
+    <p>
+      <label>Your Name: <input type="text" name="name" /></label>   
+    </p>
+    <p>
+      <label>Your Email: <input type="email" name="email" /></label>
+    </p>
+    <p>
+        <label>Your Phone Number: <input type="text" name="phone"/> </label>
+    </p>
+    <p>
+        <label>Church Website: <input type="text" name="website"/> </label>
+    </p>
+    <p>
+      <label>Message: <textarea name="message"></textarea></label>
+    </p>
+    <div data-netlify-recaptcha="true"></div>
+    <p>
+      <button type="submit">Send</button>
+    </p>
+  </form>
 )
 
 
--- src/pages/brave-work.js
+++ src/pages/brave-work.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Footer from '../components/Footer'
+import Subscription from '../components/Subscription';
 //import Subscription from '../components/Subscription'
 
 
@@ -9,29 +10,7 @@ const BraveWork = () => (
 <div>
       <h1>Brave Work</h1>
       <p>Brave Work information</p>
-      <form name="contact2" method="post" action="/success" data-netlify="true" data-netlify-honeypot="bot-field">
-        <input type="hidden" name="contact2" value="contact2" />
-        <h2>Request More Info</h2>
-        <p>
-          <label>Your Name: <input type="text" name="name" /></label>   
-        </p>
-        <p>
-          <label>Your Email: <input type="email" name="email" /></label>
-        </p>
-        <p>
-            <label>Your Phone Number: <input type="text" name="phone"/> </label>
-        </p>
-        <p>
-            <label>Church Website: <input type="text" name="website"/> </label>
-        </p>
-        <p>
-          <label>Message: <textarea name="message"></textarea></label>
-        </p>
-        <div data-netlify-recaptcha="true"></div>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
+  <Subscription/>
 </div>
 )
 
