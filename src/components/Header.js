ce2ef6bb9d35e1be6d2828cb1fa5112f07726c82
Brent Ingram <bingr001@users.noreply.github.com>
2019-02-11 09:30:44 -0800

    Changed out background position bg file and pl logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import pedro from '../images/pedro.jpg'
-import pedroLogo from '../images/PL-LOGO-min.png'
+import pedroLogo from '../images/PL-LOGO.png'
 
 console.log(pedro)
 
