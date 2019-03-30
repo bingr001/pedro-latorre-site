19fbd453e57880e1d45bfadd7a15c594a07dd5d8
brent ingram <brentingram@brents-MacBook-Pro.local>
2019-03-30 16:58:29 -0700

    fixed google analytics code

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "YUA-77404974-1",
+        trackingId: "UA-77404974-1",
         // Puts tracking script in the head instead of the body
         head: false,
         // Setting this parameter is optional
