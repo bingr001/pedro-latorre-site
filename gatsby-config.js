e4c80c98c9b496346c258fe4ecadbe9344c1474b
brent ingram <brentingram@brents-MacBook-Pro.local>
2019-03-30 17:05:06 -0700

    removed analytics options

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,18 +22,6 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-77404974-1",
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Enables Google Optimize using your container Id
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "pedrolatorre.com",
       },
     },
     'gatsby-plugin-sass',
