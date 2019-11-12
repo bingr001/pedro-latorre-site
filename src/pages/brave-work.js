d3b51075cf93897f6de51aa3c3ab10a04de25d4d
Brent Ingram <bingr001@users.noreply.github.com>
2019-11-12 12:37:01 -0800

    iframe component working with styling

--- src/pages/brave-work.js
+++ src/pages/brave-work.js
@@ -5,7 +5,7 @@ import Helmet from 'react-helmet'
 import Footer from '../components/Footer'
 import Subscription from '../components/Subscription'
 import Testimonial from '../components/Testimonial'
-import Video from '../components/Video'
+//import Video from '../components/Video'
 //import Results from '../components/Results'
 //import Results2 from '../components/Results2'
 //       <Results/>
@@ -22,12 +22,28 @@ const BraveWork = () => (
       <div>
         <h1><a href="/"> Brave Work</a></h1>
       </div>
-      <Video
-        videoSrcURL="https://www.youtube.com/embed/dQw4w9WgXcQ"
-        videoTitle="Official Music Video on YouTube"
-      />
-      <Testimonial/>
+      
 
+      <Testimonial/>
+      <div >
+      
+      <div class="projects-wrapper">
+        <h2>Videos By Pedro</h2>
+        <div class="video-container">
+          <div class="proj-box-format">
+            <iframe src="https://www.youtube.com/embed/FV3PW2boNf0" title="pedro" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+          </div>
+          <div class="proj-box-format">
+            <iframe src="https://www.youtube.com/embed/vJsV7nG7oOk" title="pedro" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+          </div>
+          <div class="proj-box-format">
+            <iframe src="https://www.youtube.com/embed/AaRYSvD8EcE" title="pedro" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+          </div>
+        </div>
+        
+      </div>
+      
+    </div>
     </Layout>
    
     <Subscription/>
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -11,16 +11,24 @@ const SecondPage = () => (
     <h1>Hi from the second page</h1>
     <Link to="/">Go back to the homepage</Link>
     <br></br>
-    <div class="projects-wrapper ">
-      <div class="proj-box-format">
-        <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="pedro" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+    <div >
+      
+      <div class="projects-wrapper">
+        <h2>Videos By Pedro</h2>
+        <div class="video-container">
+          <div class="proj-box-format">
+            <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="pedro" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+          </div>
+          <div class="proj-box-format">
+            <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="pedro" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+          </div>
+          <div class="proj-box-format">
+            <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="pedro" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+          </div>
+        </div>
+        
       </div>
-      <div class="proj-box-format">
-        <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="pedro" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-      </div>
-      <div class="proj-box-format">
-        <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="pedro" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-      </div>
+      
     </div>
 
     <Testimonial/>
