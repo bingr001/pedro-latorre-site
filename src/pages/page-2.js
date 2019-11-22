19851ed4fd899f4b64b9ea143de5c1ca2005b7cd
Brent Ingram <bingr001@users.noreply.github.com>
2019-11-21 17:02:57 -0800

    basic divs setup

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,20 +1,39 @@
 import React from 'react'
-import { Link } from 'gatsby'
+// import { Link } from 'gatsby'
 import logo from '../images/events.jpg'
 import trees from '../images/palms.jpg'
 
-import Layout from '../components/layout'
-import Subscription from '../components/Subscription'
-import Testimonial from '../components/Testimonial';
+// import Layout from '../components/layout'
+// import Subscription from '../components/Subscription'
+// import Testimonial from '../components/Testimonial';
 
 
 const SecondPage = () => (
-<div>
+<div >
+  <div class="logoHeader">
     <img style={{ maxWidth: 200}} src={logo} />
+  </div>
+  <div class ="heroContainer">
     <img style={{ maxWidth: 500 }} src={trees} />
-<h1>
-  Hello
-</h1>
+    <p>
+    Hambray keytar asymmetrical tilde, woke kombucha everyday carry vaporware viral activated charcoal pop-up lumbersexual schlitz readymade. 
+    </p>
+    <button>Click Me</button>
+  </div>
+  <div class="introVideo">
+    <iframe src="https://www.youtube.com/embed/FV3PW2boNf0" title="pedro" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+  </div>
+  <div class="cta">
+  <p>
+  Hambray keytar asymmetrical tilde, woke kombucha everyday carry vaporware viral activated charcoal pop-up lumbersexual schlitz readymade. 
+  Hambray keytar asymmetrical tilde, woke kombucha everyday carry vaporware viral activated charcoal pop-up lumbersexual schlitz readymade. 
+  Hambray keytar asymmetrical tilde, woke kombucha everyday carry vaporware viral activated charcoal pop-up lumbersexual schlitz readymade. 
+
+  </p>
+  <button>Click Me</button>
+
+  </div>
+
 </div>
 )
 
