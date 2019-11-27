9f8a25cb2241a2502d2ff1f555bc341cb5d8d56e
Brent Ingram <bingr001@users.noreply.github.com>
2019-11-27 00:20:50 -0800

    set up testimonial  and bg color  for bravework

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,121 +1,29 @@
 import React from 'react'
 // import { Link } from 'gatsby'
+import background from '../images/BG_woPedro.png'
 import logo from '../images/logo-example.jpg'
 import trees from '../images/laptop.jpg'
+import profiletemp from '../images/profiletemp.jpg'
+import anjelahProfile from '../images/anjelah.jpg'
+
+
 
 
 // import Layout from '../components/layout'
 // import Subscription from '../components/Subscription'
 // import Testimonial from '../components/Testimonial';
-const herocontainerstyle = {
-  // backgroundColor: "blue",
-  // position: "relative"
 
-  // one possible option, all in a row
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center"
-};
-
-const introvideostyle = {
-  // backgroundColor: "red"
-};
-
-const ctastyle = {
-  // backgroundColor: "pink"
-};
-
-const logoheaderstyle = {
-  maxWidth: 200,
-  
-  display: "block",
-  marginLeft: "auto",
-  marginRight: "auto"
-};
-const treestyle = {
-  paddingTop:20,
-  width: "100%",
-  
-  display: "block",
-  marginLeft: "auto",
-  marginRight: "auto"
-};
-const minicopystyle = {
- //paddingTop: 20,
-  // position: "absolute",
-  // width: "100%",
-  // top: "40%",
-  // fontSize: 18,
-  // marginLeft: 10,
-  // marginRight: 10,
-  // left: 0,
-  // maxWidth: 500
-  // transform: "translateY(10%)"
-
-  //possible route
-  // display: "flex",
-  // textAlign: "center"
-  display: "inline-block",
-  position: "absolute",
-  fontSize: "30px",
-  marginLeft: 10,
-  marginRight: 10
-  // left: "50%",
-  // right: "50%",
-  // transform: "translateY(10)"
-};
-const littleButtonstyle = {
-  //display: "block",
-  // marginLeft: "auto",
-  // marginRight: "auto",
-  // position: "absolute",
-  // top: "60%",
-  // marginLeft: "auto",
-  // marginRight: "auto",
- 
-  // right: "0%"
-  display: "inline-block",
-  position: "absolute",
-  marginBottom: -200
-  
-  
-};
-
-const bravevideostyle = {
-  paddingTop: 20,
-  paddingBottom: 20,
-  maxWidth: 1000,
-  display: "block",
-  marginLeft: "auto",
-  marginRight: "auto"
-};
-const bigcopystyle = {
-  paddingTop: 20,
-  display: "block",
-  marginLeft: "auto",
-  marginRight: "auto",
-  maxWidth: 300
-};
-const bigbuttonstyle = {
-  // paddingTop: 20,
-  display: "block",
-  marginLeft: "auto",
-  marginRight: "auto",
-  marginBottom: 100
-
-};
 
 const SecondPage = () => (
-<div >
+<div class="braveworkpage" style={braveWorkPageStyle}>
   <div class="logoHeader" >
     <img style={logoheaderstyle} src={logo} />
   </div>
   <div class="heroContainer" style={herocontainerstyle}>
     <img style={treestyle} src={trees} />
-    <p class="minicopy" style={minicopystyle}>
+    <h1 class="minicopy" style={minicopystyle}>
     Brave Work is for anyone willing to pause the pace of life and discover a new way to live.
-    </p>
+    </h1>
     <button class="littleButton" 
             style={littleButtonstyle}
             
@@ -157,17 +65,177 @@ const SecondPage = () => (
     </button>
 
   </div>
-  <div class="testimonialWrapper">
-    <div class="testimonialCard">
-      <img class="testimonyProfile"></img>
-      <p class="testimonyName"></p>
-      <p class="testimony"></p>
-
-
+  <div class="testimonialWrapper" style={testimonialWrapperStyle}>
+    <div class="testimonialCard" style={testimonialCardStyle}>
+        <img src={anjelahProfile} style={testimonyProfileStyle}></img>
+        <h2 class="testimonyName">Anjelah Johnson | Celebrity Comedian & Actress</h2>
+        <h4 class="testimony">“Pedro LaTorre managed my sold out 16 city nationwide tour. Not only did he lead our entire team with excellence, but he effectively coordinated all logistics with each venue representative. I had peace of mind knowing that Pedro was representing my company and speaking on my behalf. No matter what kind of issue arose, Pedro was sure to handle everything with professionalism and grace. I would say that Pedro’s communication and problem-solving skills are invaluable to any company.” 
+        </h4>
+    </div>
+    <div class="testimonialCard" style={testimonialCardStyle}>
+        <img src={profiletemp} style={testimonyProfileStyle}></img>
+        <h2 class="testimonyName">Brent John</h2>
+        <h4 class="testimony">Selfies vice viral, health goth vegan umami meggings austin. Succulents tumeric slow-carb mixtape waistcoat. Squid deep v green juice chia vexillologist meditation. Polaroid iceland lyft kickstarter. Twee chicharrones crucifix, cornhole wolf cred intelligentsia bespoke fashion axe direct trade shaman vinyl helvetica vape. Put a bird on it lomo tbh direct trade yuccie.
+        </h4>
+    </div>
+    <div class="testimonialCard" style={testimonialCardStyle}>
+        <img src={profiletemp} style={testimonyProfileStyle}></img>
+        <h2 class="testimonyName">Brent John</h2>
+        <h4 class="testimony">Selfies vice viral, health goth vegan umami meggings austin. Succulents tumeric slow-carb mixtape waistcoat. Squid deep v green juice chia vexillologist meditation. Polaroid iceland lyft kickstarter. Twee chicharrones crucifix, cornhole wolf cred intelligentsia bespoke fashion axe direct trade shaman vinyl helvetica vape. Put a bird on it lomo tbh direct trade yuccie.
+        </h4>
     </div>
   </div>
 
 </div>
 )
+
+//styling
+
+const braveWorkPageStyle = {
+  backgroundImage: `url(${background})`,
+  backgroundPostion: "center",
+  backgroundSize: "cover",
+  width: "100%",
+  backgroundRepeat: "no-repeat"
+}
+
+const herocontainerstyle = {
+  // backgroundColor: "blue",
+  // position: "relative"
+
+  // one possible option, all in a row
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center"
+};
+
+const introvideostyle = {
+  // backgroundColor: "red"
+};
+
+const ctastyle = {
+  // backgroundColor: "pink"
+};
+
+const logoheaderstyle = {
+  maxWidth: 200,
+
+  display: "block",
+  marginLeft: "auto",
+  marginRight: "auto"
+};
+const treestyle = {
+  paddingTop: 20,
+  width: "100%",
+
+  display: "block",
+  marginLeft: "auto",
+  marginRight: "auto"
+};
+const minicopystyle = {
+  //paddingTop: 20,
+  // position: "absolute",
+  // width: "100%",
+  // top: "40%",
+  // fontSize: 18,
+  // marginLeft: 10,
+  // marginRight: 10,
+  // left: 0,
+  // maxWidth: 500
+  // transform: "translateY(10%)"
+
+  //possible route
+  // display: "flex",
+  // textAlign: "center"
+  display: "inline-block",
+  position: "absolute",
+  textAlign: "center",
+  fontSize: 25,
+  marginLeft: 10,
+  marginRight: 10
+  // left: "50%",
+  // right: "50%",
+  // transform: "translateY(10)"
+};
+const littleButtonstyle = {
+  //display: "block",
+  // marginLeft: "auto",
+  // marginRight: "auto",
+  // position: "absolute",
+  // top: "60%",
+  // marginLeft: "auto",
+  // marginRight: "auto",
+
+  // right: "0%"
+  display: "inline-block",
+  position: "absolute",
+  marginBottom: -200
+
+
+};
+
+const bravevideostyle = {
+  paddingTop: 20,
+  paddingBottom: 20,
+  maxWidth: 1000,
+  display: "block",
+  marginLeft: "auto",
+  marginRight: "auto"
+};
+const bigcopystyle = {
+  paddingTop: 20,
+  display: "block",
+  marginLeft: "auto",
+  marginRight: "auto",
+  maxWidth: 300,
+  fontSize: 16,
+  color: "black"
+};
+const bigbuttonstyle = {
+  // paddingTop: 20,
+  display: "block",
+  marginLeft: "auto",
+  marginRight: "auto",
+  marginBottom: 100
+};
+
+const testimonialWrapperStyle = {
+  maxWidth: 980,
+  position: "relative",
+  margin: "5%",
+  paddingBottom: 90,
+  border: "(0 solidBlue)",
+  border: "(0 solidBlack)",
+  backgroundColor: "rgba(27, 31, 34, 0.85)",
+  borderRadius: 10,
+}
+
+const testimonialCardStyle = {
+  // float: "left",
+  // height: "auto",
+  // maxWidth: "80%",
+  // margin: 10,
+  // marginBottom: 30,
+  // alignItems: "center"
+  display: "grid",
+  gridTemplateColumns: "repeat(1, 3fr)",
+  textAlign: "center",
+  flexDirection: "column",
+  width: "100%",
+  margin: "(0 auto)",
+  padding: 10
+}
+
+const testimonyProfileStyle = {
+  // paddingTop: 20,
+  width: "40%",
+  display: "block",
+  borderRadius: "50%",
+  marginLeft: "auto",
+  marginRight: "auto"
+
+}
+
 
 export default SecondPage
